273a814dffe11dfaa380c0660f578d2fa92f276d
dofarifa <1939318522@qq.com>
2024-11-11 00:35:22 +0800

    Site updated: 2024-11-11 00:35:22

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/11/10/hello-world/"];function toRandomPost(){
+var posts=["2024/11/10/hello-world/","2024/11/11/测试/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
