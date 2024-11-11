242750fe89d665967232b3e38989e5cea896bfa6
dofarifa <1939318522@qq.com>
2024-11-11 10:56:49 +0000

    Delete source/_posts/牛王牛王我们喜欢你.md by Qexo Updated By Github Actions

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/11/11/hello-world/","2024/11/11/牛王牛王我们喜欢你/"];function toRandomPost(){
+var posts=["2024/11/11/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
