82a053b39825f908f75a884269ec1d08a29bbbe6
dofarifa <1939318522@qq.com>
2024-11-13 08:59:43 +0000

    Publish Post source/_posts/程序设计实验三.md by Qexo Updated By Github Actions

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/11/13/hello-world/"];function toRandomPost(){
+var posts=["2024/11/13/hello-world/","2024/11/13/程序设计实验三/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
