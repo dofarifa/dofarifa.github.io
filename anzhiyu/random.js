9071aba05efe8edd44dc9a1d1effd3ec2afb944a
dofarifa <1939318522@qq.com>
2024-11-28 05:06:02 +0000

    Publish Post source/_posts/程序设计实验五.md by Qexo Updated By Github Actions

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/11/19/一直在一起/","2024/11/13/程序设计实验三/","2024/11/19/程序设计实验四/"];function toRandomPost(){
+var posts=["2024/11/19/一直在一起/","2024/11/13/程序设计实验三/","2024/11/28/程序设计实验五/","2024/11/19/程序设计实验四/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
