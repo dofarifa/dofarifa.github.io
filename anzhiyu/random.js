ddce5cf12a7538f8c1e3db0d3f19968fc8e31e55
dofarifa <1939318522@qq.com>
2025-08-15 17:57:11 +0000

    修改音乐 Updated By Github Actions

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/08/13/代码分析/","2025/04/22/一直在一起/"];function toRandomPost(){
+var posts=["2025/04/22/一直在一起/","2025/08/13/代码分析/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
