7b2b21d2ad63682878c55f4f318e30c8ef2cddb0
dofarifa <dofarifa@users.noreply.github.com>
2026-08-28 14:49:05 +0800

    修改blog头像

--- src/consts.ts
+++ src/consts.ts
@@ -5,4 +5,4 @@ export const SITE_TAGLINE = '把复杂的事想清楚，把值得的事写下来
 
 // 图片可放入 public 目录后填写站内路径，也可以填写完整的 HTTPS 地址。
 export const SITE_ICON = '/favicon.svg';
-export const SITE_AVATAR = '/favicon.svg';
+export const SITE_AVATAR = '/avatar.jpg';
